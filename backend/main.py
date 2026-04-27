7f3e24f9988c9aacc390a8c8a95d77e2a83db61b
Larry Wang <larrywang600@gmail.com>
2026-04-27 16:01:44 -0400

    Updated CORS

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Unhinged", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://unhinged-ochre.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
